d61fff8760780900b82165bdef147de9bdfa6ecc
xhuang8 <xhuang8@mail.sfsu.edu>
2019-08-02 20:17:45 -0700

    email validate

--- web/js/validateEmail.js
+++ web/js/validateEmail.js
@@ -1,4 +1,62 @@
-function validateEmail() {
+var emailInput = document.getElementById("email");
+var one = document.getElementById("one");
+var two = document.getElementById("two");
+var three = document.getElementById("three");
+
+//show the messages when user click the email field.
+emailInput.onfocus = function ()
+{
+    document.getElementById("emailMessages").style.display = "block";
+}
+
+//hide the message when user click outside of the email field.
+emailInput.onblur = function ()
+{
+    document.getElementById("emailMessages").style.display = "none";
+}
+
+emailInput.onkeyup = function () {
+    //validate before @
+    var beforeC = /[A-Z0-9._%+-]/g;
+    if(emailInput.value.match(beforeC))
+    {
+        one.classList.remove("invalid");
+        one.classList.add("valid");
+    }
+    else
+    {
+        one.classList.remove("valid");
+        one.classList.add("invalid");
+    }
+
+    //validate after @
+    var afterC = /@[A-Z0-9.-]/g;
+    if(emailInput.value.match(afterC))
+    {
+        two.classList.remove("invalid");
+        two.classList.add("valid");
+    }
+    else
+    {
+        two.classList.remove("valid");
+        two.classList.add("invalid");
+    }
+
+    //validate .
+    var dotP = /.[A-Z]{2,}/g;
+    if(emailInput.value.match(dotP))
+    {
+        three.classList.remove("invalid");
+        three.classList.add("valid");
+    }
+    else
+    {
+        three.classList.remove("valid");
+        three.classList.add("invalid");
+    }
+}
+
+/*function validateEmail() {
     //var reg = /^([A-Za-z0-9_\-\.]){1,}\@([A-Za-z0-9_\-\.]){1,}\.([A-Za-z]{2,4})$/;
     var x = document.myform.email.value;
     var atPosition = x.indexOf("@");
@@ -13,3 +71,4 @@ function validateEmail() {
         alert("You have valid email address");
     }
 }
+*/
